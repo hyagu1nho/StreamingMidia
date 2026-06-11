c353c20f570a3b53f9a1ce94c6a4752dcdb79348
Hyago Tonelli <hyagotonelli@gmail.com>
2026-06-11 20:01:39 -0300

    update filme.cs

---
+++ Filme.cs
@@ -0,0 +1,21 @@
+namespace StreamingMidia.Domain;
+
+public class Filme : Midia
+{
+    public string Diretor { get; set; }
+    public string Classificacao { get; set; }
+
+    public Filme(string titulo, string genero, int duracaoEmMinutos, string diretor, string classificacao)
+        : base(titulo, genero, duracaoEmMinutos)
+    {
+        Diretor = diretor;
+        Classificacao = classificacao;
+    }
+
+    public override void ExibirDetalhes()
+    {
+        Console.WriteLine($" Filme: {Titulo}");
+        Console.WriteLine($"   Diretor: {Diretor} | Classificação: {Classificacao}");
+        Console.WriteLine($"   Gênero: {Genero} | Duração: {DuracaoEmMinutos} min");
+    }
+}
